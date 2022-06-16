da819df29303e8f7d7a639582b66e624de27a702
o920 <o920_@naver.com>
2022-06-16 16:22:45 +0900

    swift-const,var,int,string,double

--- hello_swift.playground/Contents.swift
+++ hello_swift.playground/Contents.swift
@@ -1,3 +1,32 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// 변수var 와 상수let
+//
+var num = 5
+num = 1 // 값 수정 가능
+//
+let name = "Jason"
+// name = "Jay" // 값 수정 불가능
+
+
+// 문자열 string
+// ""로 선언함
+let quote = "string test"
+
+// 정수
+// 소숫점 아래 버림
+let num1 = 5
+let othernum = num1 + 2
+let halfOfNum = othernum / 2
+
+let num3 = 360
+print(num3.isMultiple(of: 3)) // 3의 배수인지 확인하는 메소드
+let random = Int.random(in:0...300) // 0~300 사이 중 랜덤으로 수를 뽑아내는 메소드
+
+// 소수 double
+let score = 3.6
+// option 키 누르고 변수나 상수를 클릭하면 type이 나옴
+
+
