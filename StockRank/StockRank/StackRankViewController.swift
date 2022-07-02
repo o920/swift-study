6054acff4240b2280aa389380c1b79c19554cb1a
o920 <o920_@naver.com>
2022-07-02 10:14:36 +0900

    StockRank - cell

--- StockRank/StockRank/StackRankViewController.swift
+++ StockRank/StockRank/StackRankViewController.swift
@@ -5,25 +5,22 @@
 //  Created by 맥성연 on 2022/07/02.
 //
 
+
+// setting class & storyboard ID
+
 import UIKit
 
 class StackRankViewController: UIViewController {
-
+    
+    // connect collection view interface
+    @IBOutlet weak var CollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
