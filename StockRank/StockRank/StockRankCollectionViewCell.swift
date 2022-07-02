edc1b198d21797c8bec572daa829ed5fa1bf7844
o920 <o920_@naver.com>
2022-07-02 13:07:17 +0900

    StockRank - complete

--- StockRank/StockRank/StockRankCollectionViewCell.swift
+++ StockRank/StockRank/StockRankCollectionViewCell.swift
@@ -14,16 +14,26 @@ class StockRankCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var rankLabel: UILabel!
     @IBOutlet weak var CI: UIImageView!
     @IBOutlet weak var companyNameLabel: UILabel!
+
     @IBOutlet weak var companyPriceLabel: UILabel!
     @IBOutlet weak var diffLabel: UILabel!
-    
     // UI component data update
     
     func configure(_ stock: StockModel) {
         rankLabel.text = "\(stock.rank)"
         CI.image = UIImage(named:stock.imageName)
         companyNameLabel.text = stock.name
-        companyPriceLabel.text = "\(stock.price) 원"
+        companyPriceLabel.text = "\(convertToCurrencyFormat(price: stock.price)) 원"
+        
+        // 과제
+        diffLabel.textColor = stock.diff > 0 ? UIColor.systemRed : UIColor.systemBlue
         diffLabel.text = "\(stock.diff)%"
+    }
+    
+    func convertToCurrencyFormat(price: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.maximumFractionDigits = 0
+        return numberFormatter.string(from: NSNumber(value: price))!
     }
 }
