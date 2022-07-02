cbece44e34c730531427ae67a661100012e07c96
o920 <o920_@naver.com>
2022-07-02 11:16:52 +0900

    stockrank - cell

---
+++ StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,47 @@
+//
+//  StackRankViewController.swift
+//  StockRank
+//
+//  Created by 맥성연 on 2022/07/02.
+//
+
+
+// setting class & storyboard ID
+import UIKit
+
+class StockRankViewController: UIViewController {
+    
+    let stockList : [StockModel] = StockModel.list
+    
+    // connect collection view interface
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // Data, Presentation, Layout
+    // Data : what data to use -> stockList
+    // Presentation : how to present the cell
+    // Layout : how to position the cell
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self    // presentation
+        collectionView.delegate = self      // layout
+        // protocol
+
+    }
+
+}
+
+extension StockRankViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section : Int) -> Int{
+        return stockList.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension StockRankViewController : UICollectionViewDelegateFlowLayout{
+}
+
+}
