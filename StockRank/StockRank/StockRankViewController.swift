e067ec5660c40d4ba5613c5049d0c0ae714f805b
o920 <o920_@naver.com>
2022-07-02 12:12:29 +0900

    stockrank - cell design

---
+++ StockRank/StockRank/StockRankViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 맥성연 on 2022/07/02.
+//
+
+
+// setting class, storyboard ID @main
+import UIKit
+
+class StockRankViewController: UIViewController {
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    // Data, Presentation, Layout
+    // Data : what data to use -> stockList
+    // Presentation : how to present the cell
+    // Layout : how to position the cell
+        
+       
+    }
+}
